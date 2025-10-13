fa55c3fee02732df5ab9c2900f2374f7686d18cc
Todor Grigorov <todor.grigorov@abraxa.com>
2025-10-13 20:36:34 +0300

    port

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || '0810';
 const PUBLIC_PATH = path.join(__dirname, '..', 'public');
 
 
